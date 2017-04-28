ac421c2612bea1ba395c482a416ea2a8a62c232a
Matt Kleinschafer <xeusalmighty@gmail.com>
2017-04-28 17:03:27 +1000

    Tidy up a little.

--- graphics/math.go
+++ graphics/math.go
@@ -15,14 +15,22 @@ func V(x, y, z float64) Vector {
 	return Vector{x, y, z}
 }
 
+// Calculates the length of this vector
 func (a Vector) Length() float64 {
 	return math.Sqrt(a.X*a.X + a.Y*a.Y + a.Z*a.Z)
 }
 
+// Calculates the square length of this vector
+func (a Vector) LengthSquared() float64 {
+	return a.X*a.X + a.Y*a.Y + a.Z*a.Z
+}
+
+// Calculates the dot product of this vector and the given vector
 func (a Vector) Dot(b Vector) float64 {
 	return a.X*b.X + a.Y*b.Y + a.Z*b.Z
 }
 
+// Calculates the cross product of this vector and the given vector
 func (a Vector) Cross(b Vector) Vector {
 	x := a.Y*b.Z - a.Z*b.Y
 	y := a.Z*b.X - a.X*b.Z
@@ -30,55 +38,58 @@ func (a Vector) Cross(b Vector) Vector {
 	return V(x, y, z)
 }
 
+// Normalizes the vector
 func (a Vector) Normalize() Vector {
 	d := a.Length()
 	return V(a.X/d, a.Y/d, a.Z/d)
 }
 
+// Negates the vector
 func (a Vector) Negate() Vector {
 	return V(-a.X, -a.Y, -a.Z)
 }
 
+// Calculates the absolute value of the vector piece-wise
 func (a Vector) Abs() Vector {
 	return V(math.Abs(a.X), math.Abs(a.Y), math.Abs(a.Z))
 }
 
+// Adds the vector piece-wise to the given vector
 func (a Vector) Add(b Vector) Vector {
 	return V(a.X+b.X, a.Y+b.Y, a.Z+b.Z)
 }
 
+// Subtracts the vector piece-wise from the given vector
 func (a Vector) Sub(b Vector) Vector {
 	return V(a.X-b.X, a.Y-b.Y, a.Z-b.Z)
 }
 
+// Multiplies the vector piece-wise by the given vector
 func (a Vector) Mul(b Vector) Vector {
 	return V(a.X*b.X, a.Y*b.Y, a.Z*b.Z)
 }
 
+// Divides the vector piece-wise by the given vector
 func (a Vector) Div(b Vector) Vector {
 	return V(a.X/b.X, a.Y/b.Y, a.Z/b.Z)
 }
 
-func (a Vector) Mod(b Vector) Vector {
-	x := a.X - b.X*math.Floor(a.X/b.X)
-	y := a.Y - b.Y*math.Floor(a.Y/b.Y)
-	z := a.Z - b.Z*math.Floor(a.Z/b.Z)
-
-	return V(x, y, z)
-}
-
+// Adds the given scalar amount to the vector
 func (a Vector) AddS(b float64) Vector {
 	return V(a.X+b, a.Y+b, a.Z+b)
 }
 
+// Subtracts the given scalar amount from the vector
 func (a Vector) SubS(b float64) Vector {
 	return V(a.X-b, a.Y-b, a.Z-b)
 }
 
+// Multiplies the vector by the given scalar amount
 func (a Vector) MulS(b float64) Vector {
 	return V(a.X*b, a.Y*b, a.Z*b)
 }
 
+// Divides the vector by the given scalar amount
 func (a Vector) DivS(b float64) Vector {
 	return V(a.X/b, a.Y/b, a.Z/b)
 }
@@ -93,6 +104,33 @@ func R(origin, direction Vector) Ray {
 	return Ray{origin, direction.Normalize()}
 }
 
-func (r Ray) Position(t float64) Vector {
-	return r.Origin.Add(r.Direction.MulS(t))
+// Calculates a point along the ray with the given distance
+func (r Ray) Position(distance float64) Vector {
+	return r.Origin.Add(r.Direction.MulS(distance))
+}
+
+// Reflects the ray about the given point and normal
+func (r Ray) Reflect(point, normal Vector) Ray {
+	origin := point.Add(normal.MulS(ε))
+	direction := r.Direction.Sub(normal.MulS(2).MulS(r.Direction.Dot(normal)))
+
+	return R(origin, direction)
+}
+
+// Refracts the ray about the given point and normal
+func (r Ray) Refract(point, normal Vector, inside bool) Ray {
+	ior := 1.1
+	eta := ior
+
+	if !inside {
+		eta = 1 / ior
+	}
+
+	cosi := -normal.Dot(r.Direction)
+	k := 1 - eta*eta*(1-cosi*cosi)
+
+	origin := point.Sub(normal.MulS(Bias))
+	direction := r.Direction.MulS(eta).Add(normal.MulS(eta*cosi - math.Sqrt(k)))
+
+	return R(origin, direction)
 }
--- graphics/rendering.go
+++ graphics/rendering.go
@@ -6,8 +6,8 @@ import (
 	"image/color"
 )
 
-const MaxDepth = 3     // The maximum depth for trace recursion
-const LightBias = 1e-4 // suitable small directional bias for lighting calculations
+const MaxDepth = 3 // The maximum depth for trace recursion
+const Bias = 1e-4  // suitable small directional bias for lighting calculations
 
 // Ray-traces an RGBA image of the given dimensions using the given scene configuration
 func (scene *Scene) RayTraceToImage(dimensions image.Rectangle) (*image.RGBA) {
@@ -93,46 +93,35 @@ func (scene *Scene) trace(ray Ray, depth int, maxDepth int) (Vector) {
 
 	// compute reflection/refraction illumination up to a certain depth
 	if (material.Transparency > 0 || material.Reflectivity > 0) && depth < maxDepth {
-		// computes the fresnel lens effect for reflective/transparent surfaces;
+		// computes the fresnel lens effect for reflective/transparent surfaces
 		// see https://en.wikipedia.org/wiki/Fresnel_lens
-		mix := func(a, b, mix float64) float64 {
-			return b*mix + a*(1-mix)
+		computeFresnel := func(direction, normal Vector) float64 {
+			mix := func(a, b, mix float64) float64 {
+				return b*mix + a*(1-mix)
+			}
+
+			return mix(math.Pow(1+direction.Dot(normal), 3), 1, 0.1)
 		}
 
+		// determine whether we're inside or outside the surface
 		inside := false
 		if ray.Direction.Dot(normal) > 0 {
-			normal = normal.MulS(-1)
+			normal = normal.Negate()
 			inside = true
 		}
 
-		facingRatio := -ray.Direction.Dot(normal)
-		fresnel := mix(math.Pow(1-facingRatio, 3), 1, 0.1)
+		fresnel := computeFresnel(ray.Direction, normal)
 
-		// compute reflective color
-		reflectionDir := ray.Direction.Sub(normal.MulS(2).MulS(ray.Direction.Dot(normal)))
-		reflectionRay := R(hit.Add(normal.MulS(LightBias)), reflectionDir)
-		reflectionColor := scene.trace(reflectionRay, depth+1, maxDepth)
-
-		// compute refractive color
+		// compute reflective and refractive color by recursively tracing light along reflective and
+		// refractive angles; the combine the resultant colours
+		reflectionColor := scene.trace(ray.Reflect(hit, normal), depth+1, maxDepth)
 		refractionColor := V(0, 0, 0)
-		if material.Transparency > 0 {
-			ior := 1.1
-			eta := ior
-			if !inside {
-				eta = 1 / ior
-			}
-			cosi := -normal.Dot(ray.Direction)
-			k := 1 - eta*eta*(1-cosi*cosi)
 
-			refractionDir := ray.Direction.MulS(eta).Add(normal.MulS(eta*cosi - math.Sqrt(k)))
-			refractionRay := R(hit.Sub(normal.MulS(LightBias)), refractionDir)
-			refractionColor = scene.trace(refractionRay, depth+1, maxDepth)
+		if material.Transparency > 0 {
+			refractionColor = scene.trace(ray.Refract(hit, normal, inside), depth+1, maxDepth)
 		}
 
-		reflectiveColor := reflectionColor.MulS(fresnel)
-		refractiveColor := refractionColor.MulS(1 - fresnel).MulS(material.Transparency)
-
-		sampledColor = (reflectiveColor.Add(refractiveColor)).Mul(material.Diffuse)
+		sampledColor = reflectionColor.MulS(fresnel).Add(refractionColor.MulS(1 - fresnel).MulS(material.Transparency)).Mul(material.Diffuse)
 	} else {
 		// compute diffuse illumination, accounting for light sources and shadows
 		for _, light := range scene.Lights {
@@ -140,7 +129,7 @@ func (scene *Scene) trace(ray Ray, depth int, maxDepth int) (Vector) {
 			// the light position; we then determine whether another object occludes the light source and
 			// project a shadow if it does
 			transmission := V(1, 1, 1)
-			lightRay := R(hit.Add(normal.MulS(LightBias)), light.Position.Sub(hit))
+			lightRay := R(hit.Add(normal.MulS(Bias)), light.Position.Sub(hit))
 
 			for _, other := range scene.Objects {
 				distance := math.MaxFloat64
@@ -151,6 +140,7 @@ func (scene *Scene) trace(ray Ray, depth int, maxDepth int) (Vector) {
 				}
 			}
 
+			// compute shaded color
 			sampledColor = sampledColor.Add(material.Diffuse).Mul(transmission).MulS(math.Max(0, normal.Dot(lightRay.Direction))).Mul(light.Emission)
 		}
 	}
